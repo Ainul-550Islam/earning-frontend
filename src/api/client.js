df22a1edfad78c12abab9f6453737fe3be0d7e14
Ainul Islam <ainul10islam100@gmail.com>
2026-03-17 10:41:11 +0600

    fix: use VITE_API_BASE_URL in client.js

--- src/api/client.js
+++ src/api/client.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { setupRequestInterceptors } from './interceptors/request';
 import { setupResponseInterceptors } from './interceptors/response';
 
-const BASE_URL = 'http://127.0.0.1:8000/api/';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000/api/';
 
 const client = axios.create({
   baseURL: BASE_URL,
